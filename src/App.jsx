52a5d9bc543dcfb57022b02d7f75bc3af5a9a183
Takahito Urai <uraitakahito@gmail.com>
2024-12-09 04:44:18 +0900

    Add explanation of boolean, undefined, and null values in App component

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>
+        bool, undefined and null are ignored:
+        <ul>
+          <li>{true}</li>
+          <li>{false}</li>
+          <li>{undefined}</li>
+          <li>{null}</li>
+          <li>{0}</li>
+        </ul>
+      </p>
       <p className="read-the-docs">
         Current time:
         {(new Date()).toLocaleString()}
