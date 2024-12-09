73032206c59c09865790e662bcc1b257faef93ea
Takahito Urai <uraitakahito@gmail.com>
2024-12-09 14:06:47 +0900

    Add ESLint rule for JSX expressions and simplify greeting in PropTypesComponent

--- src/PropTypesComponent.jsx
+++ src/PropTypesComponent.jsx
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types';
 function PropTypesComponent({ name }) {
   return (
     <h2>
-      Hello,
-      {' '}
-      {name}
-      !
+      Hello, {name}!
     </h2>
   );
 }
