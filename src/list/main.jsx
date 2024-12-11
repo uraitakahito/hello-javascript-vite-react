76a5312acf89b05b28f8e3b3003dcdfd5ebb1ea9
Takahito Urai <uraitakahito@gmail.com>
2024-12-11 12:07:06 +0900

    fix: update import path for books in main.jsx

--- src/list/main.jsx
+++ src/list/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import ForList from './ForList';
-import books from './books';
+import books from '../books';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
