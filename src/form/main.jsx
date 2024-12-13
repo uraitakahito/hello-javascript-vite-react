4187529ee696ec14ee4d0a0aa048cf8de81409f1
Takahito Urai <uraitakahito@gmail.com>
2024-12-13 15:09:48 +0900

    feat: add StateNest component for nested form handling and update main.jsx to include it

--- src/form/main.jsx
+++ src/form/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import StateForm from './StateForm';
 import UncontrolledComponent from './UncontrolledComponent';
+import StateNest from './StateNest';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <StateForm />
     <UncontrolledComponent />
+    <StateNest />
   </StrictMode>,
 );
