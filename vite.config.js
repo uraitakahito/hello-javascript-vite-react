38911454f316e984677d7fbb4ac9e55e59ce0f0f
Takahito Urai <uraitakahito@gmail.com>
2024-12-15 17:22:12 +0900

    feat: update Vite configuration to include additional HTML entry points

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        button: resolve(__dirname, 'src/button/index.html'),
+        children: resolve(__dirname, 'src/children/index.html'),
+        form: resolve(__dirname, 'src/form/index.html'),
+        fromchildtoparent: resolve(__dirname, 'src/fromchildtoparent/index.html'),
+        ignore: resolve(__dirname, 'src/ignore/index.html'),
+        list: resolve(__dirname, 'src/list/index.html'),
+        renderprops: resolve(__dirname, 'src/renderprops/index.html'),
         rerender: resolve(__dirname, 'src/rerender/index.html'),
+        usememo: resolve(__dirname, 'src/usememo/index.html'),
       },
     },
     minify: false,
